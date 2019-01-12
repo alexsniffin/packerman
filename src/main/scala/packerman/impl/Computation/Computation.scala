a8fb06fd1ae52e0e2e512f0043369ae60c0f0b43
alexsniffin <alexsniffin@gmail.com>
2019-01-12 16:10:15 -0500

    Finish basic implementation

--- src/main/scala/packerman/impl/Computation/Computation.scala
+++ src/main/scala/packerman/impl/Computation/Computation.scala
@@ -1,9 +1,13 @@
 package packerman.impl.Computation
 
+import java.security.InvalidAlgorithmParameterException
+
 import packerman.impl._
 
 import scala.annotation.tailrec
 import scala.util.{Failure, Success, Try}
+
+sealed case class InputMapper[In, POut <: Double](input: In, updatedPackingProp: POut)
 
 trait ComputationMonad[In] {
   def compute(): Either[Seq[In], Error]
@@ -24,39 +28,71 @@ class Computation[In, GOut, POut <: Double](pack: Pack[In, GOut, POut]) extends 
 object Computation {
   def apply[In, GOut, POut <: Double](pack: Pack[In, GOut, POut]): Computation[In, GOut, POut] = new Computation[In, GOut, POut](pack)
 
-  def maxBinPacking[In, GOut, POut](
+  def maxBinPacking[In, GOut, POut <: Double](
       seq: Seq[In],
       groupFn: Pack.Grouping[In, GOut],
       packFn: Pack.Packing[In, POut],
-      distributeAlgorithm: DistributionAlgorithm): Seq[In] = {
+      distributeAlgorithm: DistributionAlgorithm): Seq[InputMapper[In, POut]] = {
+
+    val inputMappedSeq = seq.map(in => InputMapper(in, packFn.apply(in)))
+
     val sumOfPackValue = seq.map(packFn).reduce[Double](_ + _)
 
-    val groups: Map[GOut, Seq[In]] = seq.groupBy(groupFn)
-
-    val groupRatios = groups.map {
-      case (k: GOut, v: Seq[In]) => (k, v.map(packFn).reduce[Double](_ + _) / sumOfPackValue)
-    }
+    val groups: Map[GOut, Seq[InputMapper[In, POut]]] = inputMappedSeq.groupBy(x => groupFn(x.input))
 
     val distributed = distributeAlgorithm match {
-      case UniformDistribution(weighted, limit) => uniformDistribution(weighted, limit, sumOfPackValue, groups, groupRatios)
+      case UniformDistribution(weighted, limit) => uniformDistribution(weighted, limit, packFn, sumOfPackValue, groups)
+      case _ => throw new InvalidAlgorithmParameterException("Invalid distribution algorithm specified")
     }
 
     distributed.values.flatten.toSeq
   }
 
-  def uniformDistribution[In, GOut](
+  def uniformDistribution[In, GOut, POut <: Double](
       weighted: Boolean,
       limit: Double,
+      packFn: Pack.Packing[In, POut],
       sumOfPackValue: Double,
-      groups: Map[GOut, Seq[In]],
-      groupRatios: Map[GOut, Double]): Map[GOut, Seq[In]] = {
+      groups: Map[GOut, Seq[InputMapper[In, POut]]]): Map[GOut, Seq[InputMapper[In, POut]]] = {
+
     @tailrec
-    def reduceAndCompute(notLimited: Option[Map[GOut, Seq[In]]], limited: Option[Map[GOut, Seq[In]]] = None): Option[Map[GOut, Seq[In]]] = {
-      if (notLimited.isEmpty) return limited
+    def reduceAndCompute(
+        notLimited: Option[Map[GOut, Seq[InputMapper[In, POut]]]],
+        limited: Option[Map[GOut, Seq[InputMapper[In, POut]]]] = None): Option[Map[GOut, Seq[InputMapper[In, POut]]]] = {
+      if (notLimited.isEmpty || notLimited.get.size == 1) return Some(notLimited.get ++ limited.get)
 
-      // todo
+      val limitAmt = limit * sumOfPackValue
 
-      reduceAndCompute()
+      val groupSums: Map[GOut, Double] = groups.map {
+        case (k: GOut, v: Seq[In]) => {
+          val sumOfGroup: Double = v.map(group => packFn(group.input)).reduce[Double](_ + _)
+
+          (k, sumOfGroup)
+        }
+      }
+
+      val bucketExcessSum: Double = groupSums.filter(_._2 > limitAmt).map {
+        case (_: GOut, v: Double) => v - limitAmt
+      }.sum
+
+      val updatedlimited= Some(groupSums.filter(_._2 > limitAmt).map {
+        case (k, v) => {
+          val amtOver = v - limitAmt
+          val amtEa = amtOver / v
+
+          (k, groups(k).map(i => i.copy(updatedPackingProp = (i.updatedPackingProp - amtEa).asInstanceOf[POut])))
+        }
+      })
+
+      val updatedNotLimited = Some(groupSums.filter(_._2 <= limitAmt).map {
+        case (k, v) => {
+          val amtEa = bucketExcessSum / v
+
+          (k, groups(k).map(i => i.copy(updatedPackingProp = (i.updatedPackingProp + amtEa).asInstanceOf[POut])))
+        }
+      })
+
+      reduceAndCompute(updatedNotLimited, updatedlimited)
     }
 
     reduceAndCompute(Some(groups)).get
--- src/test/scala/packerman/PackermanSpec.scala
+++ src/test/scala/packerman/PackermanSpec.scala
@@ -6,13 +6,14 @@ import packerman.impl.UniformDistribution
 class PackermanSpec extends FlatSpec with GivenWhenThen {
   info("Running PackermanSpec")
 
-  case class Test(one: Double, two: String, three: String)
+  case class Test(one: Double, two: String)
 
   "" should "" in {
     Given("")
 
-    val packed = Packerman(List(Test(1, "ok", "no")))
+    val packed = Packerman(List(Test(1, "group1"), Test(1, "group1"), Test(1, "group2")))
       .groupBy(x => x.two)
+      //.mapInputSetBy((map, input) => map(input.one))
       .packBy(x => x.one)
       .distributionStrategy(UniformDistribution(weighted = true, 0.5))
       .compute()
