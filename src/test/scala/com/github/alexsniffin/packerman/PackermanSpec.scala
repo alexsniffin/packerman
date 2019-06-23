1e91064ceeada086b89e9bb54f29e6e4b6781287
alexsniffin <alexsniffin@gmail.com>
2019-06-23 19:13:54 -0400

    Fix POut type to be compile safe with Numeric

--- src/test/scala/com/github/alexsniffin/packerman/PackermanSpec.scala
+++ src/test/scala/com/github/alexsniffin/packerman/PackermanSpec.scala
@@ -8,31 +8,13 @@ class PackermanSpec extends FlatSpec with GivenWhenThen {
   info("Running PackermanSpec")
 
   "apply" should "return a group monad" in {
-    case class Input(key: String, value: Int)
+    Given("a seq")
+    val seq = Seq()
 
-    val collection = Seq(
-      Input("29b482bca2af868b0cc6c7c409fe3d14", 11),
-      Input("d724b748d259fa0f39a4ac776a86ad61", 2),
-      Input("df1662f0aed98c78c192654461afb6f6", 2)
-    )
+    When("creating a new instance of com.alexsniffin.packerman with apply")
+    val groupMonad = PackermanEngine.apply(seq)
 
-    val packerman = PackermanEngine(collection)
-        .groupBy(x => x.key)
-        .packBy(x => x.value)
-        .distributionStrategy(UniformDistribution(true, .5))
-        .compute()
-
-    val output = packerman match {
-      case Right(result) => result
-      case l @ Left(err) => l // handle error
-    }
-//    Given("a seq")
-//    val seq = Seq()
-//
-//    When("creating a new instance of com.alexsniffin.packerman with apply")
-//    val groupMonad = PackermanEngine.apply(seq)
-//
-//    Then("apply should return a group monad")
-//    assert(groupMonad.isInstanceOf[GroupProps[_]])
+    Then("apply should return a group monad")
+    assert(groupMonad.isInstanceOf[GroupProps[_]])
   }
 }
--- src/test/scala/com/github/alexsniffin/packerman/impl/DistributionStrategy/DistributionStrategySpec.scala
+++ src/test/scala/com/github/alexsniffin/packerman/impl/DistributionStrategy/DistributionStrategySpec.scala
@@ -5,43 +5,45 @@ import com.github.alexsniffin.packerman.impl.Computation.{Computation, UniformDi
 import com.github.alexsniffin.packerman.impl.Pack
 import org.scalatest.{FlatSpec, GivenWhenThen}
 
+import scala.math.Numeric.IntIsIntegral
+
 class DistributionStrategySpec extends FlatSpec with GivenWhenThen {
   info("Running DistributionStrategySpec")
 
-//  "apply" should "return a distribution monad" in {
-//    Given("a pack object")
-//    val pack = Pack[TestClass](Seq(TestClass("Test", 0)))
-//
-//    When("creating a new instance of distribution with apply")
-//    val distribution = DistributionStrategy.apply(pack)
-//
-//    Then("apply should return a group monad")
-//    assert(distribution.isInstanceOf[DistributionStrategy[TestClass, _, _]])
-//  }
-//
-//  "distributionStrategy" should "return a computation instance" in {
-//    Given("a distribution monad and a computation strategy")
-//    val distribution = DistributionStrategy(Pack[TestClass](Seq(TestClass("Test", 0))))
-//    val strategy: UniformDistribution = UniformDistribution(true, 1)
-//
-//    When("distributionStrategy is called with the strategy")
-//    val computation = distribution.distributionStrategy(strategy)
-//
-//    Then("it should return a computation monad")
-//    assert(computation.isInstanceOf[Computation[TestClass, _, _]])
-//  }
-//
-//  "distributionStrategy" should "return a computation instance that copied the pack object and added strategy" in {
-//    Given("a pack object, distribution monad and strategy")
-//    val pack = Pack(Seq(TestClass("Test", 0)))
-//    val distribution = DistributionStrategy(pack)
-//    val strategy: UniformDistribution = UniformDistribution(true, 1)
-//
-//    When("distributionStrategy is called with the strategy")
-//    val computation: Computation[TestClass, _, _] = distribution.distributionStrategy(strategy).asInstanceOf[Computation[TestClass, _, _]]
-//
-//    Then("the pack object should be different than the original")
-//    assert(computation.pack != pack)
-//    assert(computation.pack.distributeAlgorithm.isDefined)
-//  }
+  "apply" should "return a distribution monad" in {
+    Given("a pack object")
+    val pack = Pack.create[TestClass, Any, Int](Some(Seq(TestClass("Test", 0))), None, None)
+
+    When("creating a new instance of distribution with apply")
+    val distribution = DistributionStrategy.apply[TestClass, Any, Int](pack)
+
+    Then("apply should return a group monad")
+    assert(distribution.isInstanceOf[DistributionStrategy[TestClass, _, _]])
+  }
+
+  "distributionStrategy" should "return a computation instance" in {
+    Given("a distribution monad and a computation strategy")
+    val distribution = DistributionStrategy(Pack.create[TestClass, Any, Int](Some(Seq(TestClass("Test", 0))), None, None))
+    val strategy: UniformDistribution = UniformDistribution(true, 1)
+
+    When("distributionStrategy is called with the strategy")
+    val computation = distribution.distributionStrategy(strategy)
+
+    Then("it should return a computation monad")
+    assert(computation.isInstanceOf[Computation[TestClass, _, _]])
+  }
+
+  "distributionStrategy" should "return a computation instance that copied the pack object and added strategy" in {
+    Given("a pack object, distribution monad and strategy")
+    val pack = Pack.create[TestClass, Any, Int](Some(Seq(TestClass("Test", 0))), None, None)
+    val distribution = DistributionStrategy(pack)
+    val strategy: UniformDistribution = UniformDistribution(true, 1)
+
+    When("distributionStrategy is called with the strategy")
+    val computation: Computation[TestClass, _, _] = distribution.distributionStrategy(strategy).asInstanceOf[Computation[TestClass, _, _]]
+
+    Then("the pack object should be different than the original")
+    assert(computation.pack != pack)
+    assert(computation.pack.distributeAlgorithm.isDefined)
+  }
 }
